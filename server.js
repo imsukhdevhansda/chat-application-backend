1d66a3b14664ce14411545dd2a097778d34a5c33
Sukhdev Hansda <sukhdevhansda8@gmail.com>
2023-02-22 19:37:08 +0530

    Change api wecome title

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
-  res.send("Welcome to the todo app server");
+  res.send("Welcome to the cheat app server");
 });
 
 app.use("/login", loginRouter);
